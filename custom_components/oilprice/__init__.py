81ca8b5c84c2634072f86fb44dc83f0651993a7d
Allen <z-l-8@hotmail.com>
2025-03-21 15:21:04 +0000

    update

--- custom_components/oilprice/__init__.py
+++ custom_components/oilprice/__init__.py
@@ -1,1 +1,14 @@
+"""The Oil Price integration."""
+from .sensor import OilPriceSensor
 
+async def async_setup_entry(hass, config_entry):
+    """Set up Oil Price from a config entry."""
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(config_entry, "sensor")
+    )
+    return True
+
+async def async_unload_entry(hass, config_entry):
+    """Unload a config entry."""
+    await hass.config_entries.async_forward_entry_unload(config_entry, "sensor")
+    return True
